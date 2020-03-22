33cbc2e75a74176939254afad81ef0bc25eca38c
Richard Kendall <richard.kendall@gmail.com>
2020-03-22 20:03:14 +1100

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "{" >> "settings.json"
+echo "  \"HOST\": \"$HOST\"," >> "settings.json"
+echo "  \"REALM\": \"$REALM\"," >> "settings.json"
+echo "  \"CLIENT_ID\": \"$CLIENT_ID\"," >> "settings.json"
+echo "  \"CLIENT_SECRET\": \"$CLIENT_SECRET\"," >> "settings.json"
+echo "  \"AUTH_COOKIE\": \"$AUTH_COOKIE\"," >> "settings.json"
+echo "  \"REFRESH_COOKIE\": \"$REFRESH_COOKIE\"," >> "settings.json"
+echo "  \"VAL_API_URL\": \"$VAL_API_URL\"" >> "settings.json"
+echo "}" >> "settings.json"
